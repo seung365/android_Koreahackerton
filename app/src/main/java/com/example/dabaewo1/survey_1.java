94f465290ab977fc87e12a83593750b5efe0a158
seungbeom <bdh3659@naver.com>
2023-08-03 14:38:03 +0900

    third

---
+++ app/src/main/java/com/example/dabaewo1/survey_1.java
@@ -0,0 +1,14 @@
+package com.example.dabaewo1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class survey_1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_survey1);
+    }
+}
