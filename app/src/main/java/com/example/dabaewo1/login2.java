563134cc60ab06e7e66314a2a1be1feb53849c76
heonniy <dkgus7535@naver.com>
2023-08-03 12:08:08 +0900

    프론트-주소 update12

--- app/src/main/java/com/example/dabaewo1/login2.java
+++ app/src/main/java/com/example/dabaewo1/login2.java
@@ -1,29 +1,27 @@
-package com.example.dabaewo1;// login2.java
+package com.example.dabaewo1;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.dabaewo1.searchAddress;
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class login2 extends AppCompatActivity {
     private static final int REQUEST_SEARCH_ADDRESS = 1;
-    private TextInputLayout loginAddField;
+    private TextInputEditText loginAddField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login2);
 
-        loginAddField = findViewById(R.id.login_add_field);
+        loginAddField = findViewById(R.id.add);
 
-        ImageButton btnSearchAddress = findViewById(R.id.myButton);
-        btnSearchAddress.setOnClickListener(new View.OnClickListener() {
+        loginAddField.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // 주소 검색 웹뷰 화면으로 이동
@@ -38,10 +36,9 @@ public class login2 extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_SEARCH_ADDRESS && resultCode == RESULT_OK && data != null) {
             String address = data.getStringExtra("data");
-            // 여기서 주소 검색 결과 처리
-            EditText editText = loginAddField.getEditText();
-            if (editText != null && address != null && !address.isEmpty()) {
-                editText.setText(address);
+            // 주소 검색 결과 처리
+            if (loginAddField != null && address != null && !address.isEmpty()) {
+                loginAddField.setText(address);
             }
         }
     }
