fb67d89276c87f50869c148c03fdfe352b045d67
heonniy <dkgus7535@naver.com>
2023-08-10 17:27:41 +0900

    -d

--- app/src/main/java/com/example/dabaewo1/MainMenuActivity.java
+++ app/src/main/java/com/example/dabaewo1/MainMenuActivity.java
@@ -13,10 +13,11 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class MainMenuActivity extends AppCompatActivity {
 
     private FragmentManager fragmentManager = getSupportFragmentManager();
-    private mainhome fragmentChart = new mainhome();
-    private mainsearch fragmentSearch = new mainsearch();
-    private mainreserve fragmentReserve = new mainreserve();
-    private mainmypage fragmentMypage = new mainmypage();
+
+    private MainMenuHomeFragment fragmentHome = new MainMenuHomeFragment();
+    private MainMenuSearchFragment fragmentSearch = new MainMenuSearchFragment();
+    private MainMenuMypageFragment fragmentMypage = new MainMenuMypageFragment();
+    private MainMenuReserveFragment fragmentReserve = new MainMenuReserveFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +25,10 @@ public class MainMenuActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_menu);
 
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.menu_frame_layout, fragmentChart).commitAllowingStateLoss();
+        transaction.replace(R.id.menu_frame_layout, fragmentHome).commitAllowingStateLoss();
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.menu_bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
-
     }
 
     class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener {
@@ -36,18 +36,18 @@ public class MainMenuActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
             FragmentTransaction transaction = fragmentManager.beginTransaction();
 
-            switch (menuItem.getItemId()) {
-                case R.id.home:
-                    transaction.replace(R.id.menu_frame_layout, fragmentChart).commitAllowingStateLoss();
-                    break;
-                case R.id.search:
-                    transaction.replace(R.id.menu_frame_layout, fragmentSearch).commitAllowingStateLoss();
-                    break;
-                case R.id.reserve:
-                    transaction.replace(R.id.nevigation_menu, fragmentReserve).commitAllowingStateLoss();
-                    break;
-
+            int itemId = menuItem.getItemId();
+            if (itemId == R.id.menu_home) {
+                transaction.replace(R.id.menu_frame_layout, fragmentHome).commitAllowingStateLoss();
+            } else if (itemId == R.id.menu_search) {
+                transaction.replace(R.id.menu_frame_layout, fragmentSearch).commitAllowingStateLoss();
+            } else if (itemId == R.id.menu_reserve) {
+                transaction.replace(R.id.menu_frame_layout, fragmentReserve).commitAllowingStateLoss();
+            } else if (itemId == R.id.menu_mypage) {
+                transaction.replace(R.id.menu_frame_layout, fragmentMypage).commitAllowingStateLoss();
             }
+
+            // 다른 case 문을 추가하세요.
 
             return true;
         }
