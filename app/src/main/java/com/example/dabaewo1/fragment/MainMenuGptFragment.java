3627a88a43375b8a8032db8488d608576671162e
seungbeom <bdh3659@naver.com>
2023-08-11 21:51:12 +0900

    0811_gpt_fragment_1

--- app/src/main/java/com/example/dabaewo1/fragment/MainMenuGptFragment.java
+++ app/src/main/java/com/example/dabaewo1/fragment/MainMenuGptFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -14,7 +17,18 @@ public class MainMenuGptFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_main_menu_gpt, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_main_menu_gpt, container, false);
+
+        final EditText input = rootView.findViewById(R.id.text_input);
+        Button print_button = rootView.findViewById(R.id.button_text01);
+        final TextView output = rootView.findViewById(R.id.text_output);
+
+        print_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                output.setText(input.getText());
+            }
+        });
 
         return rootView;
     }
