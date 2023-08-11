f88c5aaccfbd1337d9f5554bac2a726d2880b281
seungbeom <bdh3659@naver.com>
2023-08-11 16:44:31 +0900

    0811_gpt_activity

--- app/src/main/java/com/example/dabaewo1/MainMenuHomeFragment.java
+++ app/src/main/java/com/example/dabaewo1/MainMenuHomeFragment.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.content.Intent;
 import androidx.fragment.app.Fragment;
 
 public class MainMenuHomeFragment extends Fragment {
@@ -12,10 +12,16 @@ public class MainMenuHomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup)inflater.inflate(R.layout.fragment_main_menu_home, container, false);
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_main_menu_home, container, false);
+
+        // OneActivity를 자동으로 호출하는 코드
+        Intent intent = new Intent(getActivity(), gpt_1.class);
+        startActivity(intent);
 
         return rootView;
     }
+
 
 
 
